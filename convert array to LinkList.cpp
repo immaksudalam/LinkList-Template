00cbcab7edbc6eae8ae7fac832608f8985a6f42d
Maksud Alam Rony <53002641+immaksudalam@users.noreply.github.com>
2022-04-26 22:02:59 -0700

    Update convert array to LinkList.cpp

--- convert array to LinkList.cpp
+++ convert array to LinkList.cpp
@@ -21,6 +21,21 @@ void insertAtTail(node* &head, int val){
 	}
 	temp->next = n;
 }
+void insertAtHead(node* &head, int val){
+	node *n = new node(val);
+	n->next = head;
+	head = n;
+}
+void insertAfterValue(node* &head, int target, int val){
+	node *temp = head;
+	node *n = new node(val);
+	while(temp->val != target){
+		temp = temp->next;
+	}
+	node *t = temp->next;
+	temp->next = n;
+	n->next = t;
+}
 int main()
 {
 	int n;
@@ -32,6 +47,8 @@ int main()
 		insertAtTail(head, v[i]);
 	}
 	insertAtTail(head, 6);
+	insertAtHead(head, 10);
+	insertAfterValue(head, 4, 20);
 	while(head){
 		if(head->next)cout<<head->val<<"-> ";
 		else cout<<head->val;
